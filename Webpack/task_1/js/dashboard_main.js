56455ccff770bcfc8d3ee600f3c27b5f7f013985
yelsy <yelsycg17@gmail.com>
2023-11-11 00:29:14 -0500

    feat: Add elements to the body

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function () {
+  // Add elements to the body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="startBtn">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+  const updateCounter = _.debounce(function () {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }, 1000);
+
+  $('#startBtn').on('click', updateCounter);
+});
