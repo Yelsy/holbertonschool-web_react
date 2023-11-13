63cb4270efba1672f947899dfa2a1d65f74cb5b4
yelsy <yelsycg17@gmail.com>
2023-11-13 18:26:02 -0500

    add body file js

---
+++ Webpack/task_3/modules/body/body.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import './body.css'
+
+$(document).ready(function () {
+  // Add elements to the body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="startBtn">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+  const updateCounter = _.debounce(function () {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }, 500);
+
+  $('#startBtn').on('click', updateCounter);
+});
