c5297d89556134b4fd0da5f068eac626cc4a0d81
yelsy <yelsycg17@gmail.com>
2023-11-21 18:25:05 -0500

    feat: function that returns a string

---
+++ react_intro/task_3/dashboard/src/utils.js
@@ -0,0 +1,17 @@
+
+export const getFullYear = () => {
+    const currentYear = new Date().getFullYear();
+    return currentYear;
+}
+
+export const getFooterCopy = (isIndex) => {
+    if (isIndex) {
+      return 'Holberton School'
+    }else {
+     return 'Holberton School main dashboard'
+    }
+}
+
+export const getLatestNotification = () => {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+}
