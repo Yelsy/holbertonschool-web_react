2c7666b6d5493570a8493657bcc85f1424a152cb
yelsy <yelsycg17@gmail.com>
2023-11-28 23:25:44 -0500

    feat: setup config

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,42 +1,42 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+console.log('__dirname:', __dirname);
 module.exports = {
   mode: 'development',
-	devtool: 'inline-source-map',
+  devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+	filename: 'bundle.js',
   },
 	devServer: {
 		hot: true,
-		contentBase: path.resolve(__dirname, 'dist'),
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
 	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: './dist/index.html',
-      title: 'Holberton Dashboard',
-    }),
-	],
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader',
+				use: {
+					loader: 'babel-loader'
+				}
 			},
 			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
+				test: /\.(png|svg|jpg|jpeg|gif)$/,
+				use: [
+					'file-loader?name=assets/[name].[ext]'
+				]
 			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
-				loader: 'image-webpack-loader',
-			},
+			/*{
+				test: /\.(html)$/,
+				use: 'html-loader'
+			},*/
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+              },
 		],
 	},
 };
