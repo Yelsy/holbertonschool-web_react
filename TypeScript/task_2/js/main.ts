a66029eb5999f613387d4161faa96105fa4065f1
yelsy <yelsycg17@gmail.com>
2023-11-30 00:52:56 -0500

    feat:Advanced types Part 1

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,40 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeBreak(): string;
+  workDirectorTasks(): string;   
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeBreak(): string;
+  workTeacherTasks(): string;   
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string) {
+  if (typeof salary === 'number' && salary < 500) return new Teacher();
+  else return new Director();
+}
