a257da6bcb1da99633e392f76751d294c0bc3c70
yelsy <yelsycg17@gmail.com>
2023-11-30 00:58:32 -0500

    String literal types

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -40,10 +40,36 @@ function createEmployee(salary: number | string) {
 }
 
 function isDirector(employee: any): employee is Director {
-  return employee instanceof Director;
+    return employee instanceof Director;
 }
 
 function executeWork(employee: Director | Teacher) {
-if (isDirector(employee)) console.log(employee.workDirectorTasks());
-else console.log(employee.workTeacherTasks());
+  if (isDirector(employee)) console.log(employee.workDirectorTasks());
+  else console.log(employee.workTeacherTasks());
 }
+
+//literal type
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects) {
+  if (todayClass === "Math") {
+    console.log("Teaching Math");
+  } else {
+    console.log("Teaching History");
+  }
+}
+
+
+//Exercise 7:
+teachClass('Math'); // Teaching Math
+teachClass('History'); // Teaching History
+
+
+//Exercise 6
+executeWork(createEmployee(200)); // Getting to work
+executeWork(createEmployee(1000)); //Getting to director tasks
+
+//Exercise 5
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
