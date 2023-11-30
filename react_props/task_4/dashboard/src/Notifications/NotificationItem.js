6a3c8b1eb710bc4937d0a0acbdf54bc4bbca5e03
yelsy <yelsycg17@gmail.com>
2023-11-29 23:42:17 -0500

    feat: update notification item

---
+++ react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const  NotificationItem = ({ type = "default", html, value }) => {
+  return (
+    <li data-notification-type={type} dangerouslySetInnerHTML={html}>
+      {value}
+    </li>
+  );
+}
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string.isRequired,
+  }),
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+};
+
+export default NotificationItem;
