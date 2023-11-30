5626670d3c31eef7aaf7332d972c4ce7399455e9
yelsy <yelsycg17@gmail.com>
2023-11-30 00:36:42 -0500

    feat: create interface named Teacher

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,46 @@
+interface Teacher {
+  //set during initialization of an object, readonly
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number, //Optional
+  location: string,
+  [key: string]: any,
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+interface printTeacherFunction {
+  firstName: string, 
+  lastname: string,
+}
+
+function printTeacher (firstName: Teacher["firstName"], lastName: Teacher["lastName"]) {
+  const sliptFirstName = firstName.split('');
+  const firstElement = sliptFirstName[0];
+  return (`${firstElement}.${lastName}`);
+}
+
+interface IStudentClass {
+  firstName: string,
+  lastName: string,
+}
+
+class StudentClass implements IStudentClass{
+  firstName: string;
+  lastName: string;
+
+  constructor (firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+ 
+  workOnHomework() {
+    return "Currently working";
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
