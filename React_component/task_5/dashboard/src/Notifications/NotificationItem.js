0f96c314720d4a1a0d41ac75e248c50291959cdb
yelsy <yelsycg17@gmail.com>
2023-11-30 22:06:08 -0500

    Declare a pure component

---
+++ React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends PureComponent {
+	render() {
+		let {
+			id,
+			type,
+			value,
+			html,
+			markAsRead
+		} = this.props;
+
+		return (
+			<>
+				{
+					html !== undefined &&
+					<li
+						onClick={() => markAsRead(id)}
+						data-priority-type={type}
+						dangerouslySetInnerHTML={html}
+					/>
+				}
+				{
+					html === undefined &&
+					<li
+						onClick={() => markAsRead(id)}
+						data-priority-type={type}
+					>
+						{value}
+					</li>
+				}
+			</>
+		);
+	};
+};
+
+NotificationItem.propTypes = {
+	html: PropTypes.shape({
+		__html: PropTypes.string,
+	}),
+	type: PropTypes.string.isRequired,
+	value: PropTypes.string,
+	markAsRead: PropTypes.func,
+};
+
+NotificationItem.defaultProps = {
+	type: "default",
+};
+
+export default NotificationItem;
